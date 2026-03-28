298127c32cd0a27bc9049aedfaea777bf8bb5fdd
Matheus De Lima Freire <matheus.delimafreire@outlook.com>
2026-03-28 23:17:30 +0000

    asked which teams are playing in the provided image

--- main.py
+++ main.py
@@ -3,7 +3,21 @@ client = OpenAI()
 
 response = client.responses.create(
     model="gpt-4o-mini",
-    input="Write a one-sentence bedtime story about a unicorn."
+    input=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "input_text",
+                    "text": "What teams are playing in this image?",
+                },
+                {
+                    "type": "input_image",
+                    "image_url": "https://api.nga.gov/iiif/a2e6da57-3cd1-4235-b20e-95dcaefed6c8/full/!800,800/0/default.jpg"
+                }
+            ]
+        }
+    ]
 )
 
 print(response.output_text)
