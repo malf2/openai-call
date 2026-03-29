73edb3436d968aec96d01755fb298488e6c3f48e
Matheus De Lima Freire <matheus.delimafreire@outlook.com>
2026-03-29 00:09:29 +0000

    built first agents

---
+++ first_agent.py
@@ -0,0 +1,26 @@
+from agents import Agent, Runner
+import asyncio
+
+spanish_agent = Agent(
+    name="Spanish agent",
+    instructions="You only speal Spanish."
+)
+
+english_agent = Agent(
+    name="English agent",
+    instructions="You only speak English."
+)
+
+triage_agent = Agent(
+    name="Triage agent",
+    instructions="Handoff to the appropriate agent based on the language of the request.",
+    handoffs=[spanish_agent, english_agent]
+)
+
+async def main():
+    result = await Runner.run(triage_agent, input="Hi, how are you?")
+    print(result.final_output)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
